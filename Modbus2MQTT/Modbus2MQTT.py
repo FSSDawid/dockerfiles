029c50be64529ba7683c3af92cde3d75c9bffb55
Dawid Kuboszek <dk@Dawids-MacBook-Pro.local>
2018-05-14 13:11:05 +0200

    new version

--- Modbus2MQTT/Modbus2MQTT.py
+++ Modbus2MQTT/Modbus2MQTT.py
@@ -20,8 +20,11 @@ def createEmptyInstance():
     dic['TBo2'] = -12345
     dic['TPowCO'] = -12345
     dic['TCo1'] = -12345
+    dic['TCo2'] = -12345
     dic['TEko'] = -12345
+    dic['TEko2'] = -12345
     dic['TKmf'] = -12345
+    dic['TKmf2'] = -12345
     dic['Tryb'] = -12345
     return dic
 
@@ -35,7 +38,7 @@ def dirtyFixForuint16Toint32(d):
 
 def publishData(T):
     global MBD
-    for key, value in T.iteritems():
+    for key, value in T.items():
         if not MBD[key] == value:
             value = dirtyFixForuint16Toint32(int(value))
             client.publish("Frisko/Temp/%s" % key, value / 10.0, 0, True)
@@ -49,11 +52,11 @@ def readHoldingRegistersLoop():
     #Check if we have anything to write if YES then write
     global WREG
     if len(WREG) > 0:
-        for key, value in WREG.iteritems():
+        for key, value in WREG.items():
             mbc.write_register(key, value)
         WREG = {}
 
-    rr = mbc.read_holding_registers(4070, 6)
+    rr = mbc.read_holding_registers(4070, 7)
     MBDT = {}
     MBDT['TKol'] = rr.registers[0] #4070
     MBDT['TZew'] = rr.registers[1] #4071
@@ -61,9 +64,15 @@ def readHoldingRegistersLoop():
     MBDT['TBo2'] = rr.registers[3] #4073
     MBDT['TPowCO'] = rr.registers[4] #4074
     MBDT['TCo1'] = rr.registers[5] #4075
+    MBDT['TCo2'] = rr.registers[6] #4076 
     rr = mbc.read_holding_registers(4178, 2)
     MBDT['TEko'] = rr.registers[0] * 10 #4178
     MBDT['TKmf'] = rr.registers[1] * 10 #4179
+
+    rr = mbc.read_holding_registers(4188, 2)
+    MBDT['TEko2'] = rr.registers[0] * 10 #4188
+    MBDT['TKmf2'] = rr.registers[1] * 10 #4189
+
     #rr = mbc.read_holding_registers(4178, 1)
     #print rr.registers
 
@@ -73,7 +82,7 @@ def readHoldingRegistersLoop():
     publishData(MBDT)
     global MBD
     MBD = MBDT
-    Timer(0.5, readHoldingRegistersLoop).start()
+    Timer(1, readHoldingRegistersLoop).start()
 
 # The callback for when the client receives a CONNACK response from the server.
 
@@ -86,32 +95,55 @@ def on_connect(client, userdata, flags, rc):
     # reconnect then subscriptions will be renewed.
     client.subscribe("OH/Temp/TKmf")
     client.subscribe("OH/Temp/Tryb")
+    client.subscribe("Frisko/Temp/TrybTxt")
 
 # The callback for when a PUBLISH message is received from the server.
 
+global CURRState
 
 def on_message(client, userdata, msg):
-    print(msg.topic+" "+str(msg.payload))
+    #print(client.client_id)
+    #if client.client_id == 'Modbus2MQTT': return
+
+    payload = str(msg.payload).strip('"').strip('b').strip('\'')
+    print(msg.topic+" "+payload)
     if msg.topic == "OH/Temp/TKmf":
-        writeRegister(4179, int(float(str(msg.payload))))
+        writeRegister(4179, int(float(str(payload))))
 
     if msg.topic == "OH/Temp/Tryb":
-        writeRegister(4173, int(float(str(msg.payload))))
+        writeRegister(4173, int(float(payload)))
         #mbc.write_register(4179, int(float(str(msg.payload))))
+        if payload == '2': 
+            client.publish("Frisko/Temp/TrybTxt", 'Auto', 0, True)
+            CURRState['TrybTxt'] = 'Auto'
+        elif payload == '1': 
+            client.publish("Frisko/Temp/TrybTxt", 'On', 0, True)
+            CURRState['TrybTxt'] = 'On'
+        elif payload == '0': 
+            client.publish("Frisko/Temp/TrybTxt", 'Off', 0, True)
+            CURRState['TrybTxt'] = 'Off'
+
+    if msg.topic == "Frisko/Temp/TrybTxt":
+        payload = str(payload)
+        if CURRState['TrybTxt'] == payload: return
+        if payload == 'Auto': client.publish("OH/Temp/Tryb", '2', 0, True)
+        elif payload == 'On': client.publish("OH/Temp/Tryb", '1', 0, True)
+        elif payload == 'Off': client.publish("OH/Temp/Tryb", '0', 0, True)
 
 
 def on_publish(pahoClient, packet, mid):
 # Once published, disconnect
-        print "Published"
+        print ("Published")
         #pahoClient.disconnect()
 
 if __name__ == '__main__':
+    CURRState = {}
     logging.basicConfig()
     log = logging.getLogger()
     log.setLevel(logging.WARNING)
     Defaults.Timeout = 10
     Defaults.UnitId = 1
-    modBusIP = os.environ.get('MODBUS_IP')
+    modBusIP = os.environ.get('MODBUS_IP','10.0.1.190')
     modBusPort = os.getenv('MODBUS_PORT',502)
     mbc = ModbusClient(modBusIP, modBusPort, framer=ModbusFramer)
     mbc.connect()
@@ -122,7 +154,7 @@ if __name__ == '__main__':
     client.on_publish = on_publish
     mqttUser = os.environ.get('MQTT_USER')
     mqttPass = os.environ.get('MQTT_PASS')
-    mqttIP = os.environ.get('MQTT_IP')
+    mqttIP = os.environ.get('MQTT_IP','10.0.1.250')
     mqttPort = os.getenv('MQTT_PORT',1883)
     client.username_pw_set(mqttUser,mqttPass)
     client.connect(mqttIP, mqttPort, 60)
